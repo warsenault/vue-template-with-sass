a2dbca383affe17f2c37d0781d7ac9bb148a751f
Jean-Pierre Bécotte <jp.becotte@cmaisonneuve.qc.ca>
2017-10-25 09:34:32 -0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>Le compteur est à {{ compteur }}</h1>
+    <button class="button is-primary" @click="compteur++">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      compteur: 0,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
